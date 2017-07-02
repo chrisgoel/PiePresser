5778c50d4aae720e4a25553750f243f15a501bd5
Ryan Woodard <ryman559@hotmail.com>
2017-07-02 16:23:41 -0400

    Saves name

--- app/src/main/java/cutlassstudios/com/piepresser/MainActivity.java
+++ app/src/main/java/cutlassstudios/com/piepresser/MainActivity.java
@@ -1,68 +1,101 @@
-package cutlassstudios.com.piepresser;
+    package cutlassstudios.com.piepresser;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
+    import android.content.Context;
+    import android.content.SharedPreferences;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.EditText;
+    import android.widget.ImageButton;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import static java.security.AccessController.getContext;
+    import static java.security.AccessController.getContext;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
-    //EditText is basically just a field of text that the user can change with what they want.
-    //In the app, it is that space where you saw "Enter Domo Name"
-    private EditText nameBox;
-    private String name;
+        //EditText is basically just a field of text that the user can change with what they want.
+        //In the app, it is that space where you saw "Enter Domo Name"
+        private EditText nameBox;
+        private String name;
 
-    //ImageButton is the pie image
-    private ImageButton pie;
-    private int counter = 0;
 
-    //onCreateMethod is run infinitely
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //Assigning a reference to the XML file that holds the locations of each widget (EditText, Buttons, etc)
-        setContentView(R.layout.activity_main);
-        nameBox = (EditText)findViewById(R.id.editText);
-        pie = (ImageButton)findViewById(R.id.imageButton);
-        pie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                counter = getScore(getApplicationContext());
-                counter++;
+        //ImageButton is the pie image
+        private ImageButton pie;
+        private int counter = 0;
 
-                name = nameBox.getText().toString();
-                System.out.println(name);
+        //onCreateMethod is run infinitely
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //Assigning a reference to the XML file that holds the locations of each widget (EditText, Buttons, etc)
+            setContentView(R.layout.activity_main);
+            nameBox = (EditText)findViewById(R.id.editText);
+            pie = (ImageButton)findViewById(R.id.imageButton);
 
-                //Creating a popup message that notifies the user of the number of pies
-                Context context = getApplicationContext();
-                saveScore(context, counter);
-                CharSequence text = "Greetings, " + name + "! You now have " + counter + " pies.";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        });
+            //sets the name in the text box
+            nameBox.setText(getName(getApplicationContext()), TextView.BufferType.EDITABLE);
+
+            //things that happen when the thing is clicked
+            pie.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //sets the score to the previous save
+                    counter = getScore(getApplicationContext());
+                    counter++;
+
+                    //sets name to previous save
+                   // name = getName(getApplicationContext());
+
+                    name = nameBox.getText().toString();
+
+                    //Creating a popup message that notifies the user of the number of pies
+                    Context context = getApplicationContext();
+                    saveScore(context, counter);
+                    CharSequence text = "Greetings, " + name + "! You now have " + counter + " pies.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+
+
+                    saveName(getApplicationContext(),name);
+
+                }
+            });
+        }
+
+    //saves the score
+    public void saveScore(Context context,int pies){
+
+        SharedPreferences score = context.getSharedPreferences("Scores",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = score.edit();
+        editor.putInt("score",pies);
+        editor.commit();
+    }
+    //gets the current number of pies
+    public int getScore(Context context){
+
+            SharedPreferences score = context.getSharedPreferences("Scores", Context.MODE_PRIVATE);
+            return score.getInt("score", 0); //0 is the value it returns if shit goes south
+
     }
 
+    //saves the name
+        public void saveName(Context context,String name){
 
-public void saveScore(Context context,int pies){
+            SharedPreferences nameSave= context.getSharedPreferences("Names",Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = nameSave.edit();
+            editor.putString("name",name);
+            editor.commit();
+        }
 
-    SharedPreferences score = context.getSharedPreferences("Scores",Context.MODE_PRIVATE);
-    SharedPreferences.Editor editor = score.edit();
-    editor.putInt("score",pies);
-    editor.commit();
-}
-public int getScore(Context context){
+        //returns the name that was saved
+        public String getName(Context context){
 
-        SharedPreferences score = context.getSharedPreferences("Scores", Context.MODE_PRIVATE);
-        return score.getInt("score", 0); //0 is the value it returns if shit goes south
+            SharedPreferences nameSave = context.getSharedPreferences("Names", Context.MODE_PRIVATE);
+            return nameSave.getString("name", "Enter Domo Name"); //Enter Domo Name is the name it will originally show when first starting
 
-}
+        }
 
-}
+    }
